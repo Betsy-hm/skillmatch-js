56c86b88dcaa3b2ca1997df9851847d8ba1ab60f
Betsy-hm <betsybel.hm@gmail.com>
2026-05-24 03:56:14 -0300

    feat: substituir vagas por instancias de classes

--- skillmatch.js
+++ skillmatch.js
@@ -20,27 +20,29 @@ const candidato = {
 // Classe Vaga com constructor, atributos e metodos
 class Vaga {
   constructor(empresa, cargo, requisitos, descricao) {
-    this.empresa    = empresa;
+    this.empresa    = empresa;     // uso de this
     this.cargo      = cargo;
     this.requisitos = requisitos;
     this.descricao  = descricao;
   }
 
+  // Método: exibe resumo da vaga
   exibirResumo() {
     console.log("\n" + this.empresa + " | " + this.cargo);
     console.log("Requisitos: " + this.requisitos.join(", "));
     console.log(this.descricao);
   }
 
+ // Método: calcula % de compatibilidade usando habilidades do candidato
   calcularCompatibilidade(habilidadesCandidato) {
-    const possuidas = this.requisitos.filter(req =>
+    const possuidas = this.requisitos.filter(req =>   // arrow fn + filter
       habilidadesCandidato.includes(req)
     );
     return Math.round((possuidas.length / this.requisitos.length) * 100);
   }
 }
 
-// RF10: Heranca com VagaFrontEnd extends Vaga
+// Heranca com VagaFrontEnd extends Vaga
 class VagaFrontEnd extends Vaga {
   constructor(empresa, cargo, requisitos, descricao, framework) {
     super(empresa, cargo, requisitos, descricao);
@@ -48,19 +50,38 @@ class VagaFrontEnd extends Vaga {
   }
 
   exibirResumo() {
-    super.exibirResumo();
+    super.exibirResumo();                          // método herdado
     console.log("Framework: " + this.framework);
   }
 }
-// Lista de vagas (objetos literais por enquanto)
+
+// Lista de vagas como instancias das classes
 const vagas = [
-  { empresa: "TechNova",   cargo: "Dev Front-End Junior",    requisitos: ["HTML","CSS","JavaScript","React","Git"]                  },
-  { empresa: "WebCraft",   cargo: "Front-End Developer Jr",  requisitos: ["HTML","CSS","JavaScript","TypeScript","Git"]             },
-  { empresa: "PixelSoft",  cargo: "Desenvolvedor Front-End", requisitos: ["HTML","CSS","JavaScript","Vue.js","Figma","Git"]         },
-  { empresa: "DataBridge", cargo: "Dev Front Junior",        requisitos: ["HTML","CSS","JavaScript","Bootstrap","REST APIs"]        },
-  { empresa: "CloudBase",  cargo: "Front-End Junior",        requisitos: ["HTML","CSS","JavaScript","React","TypeScript","Node.js"] }
+  new VagaFrontEnd(
+    "TechNova", "Dev Front-End Júnior",
+    ["HTML", "CSS", "JavaScript", "React", "Git"],
+    "Startup de fintech em rápido crescimento.", "React"
+  ),
+  new VagaFrontEnd(
+    "WebCraft", "Front-End Developer Jr",
+    ["HTML", "CSS", "JavaScript", "TypeScript", "Git"],
+    "Agência digital especializada em e-commerce.", "Nenhum"
+  ),
+   new VagaFrontEnd(
+    "PixelSoft", "Desenvolvedor Front-End Jr",
+    ["HTML", "CSS", "JavaScript", "Vue.js", "Figma", "Git"],
+    "Empresa focada em design de produto.", "Vue.js"
+  ),
+  new Vaga(
+    "DataBridge", "Dev Front Júnior",
+    ["HTML", "CSS", "JavaScript", "Bootstrap", "REST APIs"],
+    "Empresa de analytics e visualização de dados."
+  ),
+  new Vaga(
+    "CloudBase", "Front-End Júnior",
+    ["HTML", "CSS", "JavaScript", "React", "TypeScript", "Node.js"],
+    "Scale-up de infraestrutura em nuvem."
+  )
 ];
 
-console.log("Candidato:", candidato.nome);
-console.log("Vagas encontradas:", vagas.length);
 
